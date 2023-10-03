aa662addf15980839f5411d08c44f3f73fcc71f6
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-10-03 14:13:44 -0400

    libraries

--- CreatePDFN.R
+++ CreatePDFN.R
@@ -1,1 +1,11 @@
+#module load gcc python r/4.2.2
+#source venv/bin/activate
+
+library("reticulate")
+library("tidyverse")
+library("RANN")
+library("qrng")
+library("readr")
+library("ggthemes")
+
 
