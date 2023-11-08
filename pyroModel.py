744b7163ea70afd8687476f19f2d7aa86419379c
Isabella C. Penney <95446790+isabellajchiara@users.noreply.github.com>
2023-11-08 12:32:51 -0500

    2fc model

--- pyroModel.py
+++ pyroModel.py
@@ -4,106 +4,46 @@ import pyro
 
 ## create NN
 
-class NeuralNetwork(nn.Module):
-    def __init__(self, in_size, out_size):
+class BayesianNetwork(nn.Module):
+    def __init__(self, model_spec, device='cuda'):
         super().__init__()
-        self.weight = nn.Parameter(torch.randn(in_size, out_size))
-        self.bias = nn.Parameter(torch.randn(out_size))
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(),
-            nn.ReLU(),
-            nn.Linear(),
-            nn.ReLU(),
-            nn.Linear()
-        )
-        super().__init__()
-        self.weight = nn.Parameter(torch.randn(in_size, out_size))
-        self.flatten = nn.Flatten()
-        self.bias = nn.Parameter(torch.randn(out_size))
+
+        self.device = device
+        self.num_features = modelspec['num_features']
+
+        mu, sigma = torch.tensor([0.0]).to(self.device), torch.tensor([1.0]).to(self.device)
+
+        self.fc1 = PyroModule[nn.Linear](self.num_features, model_spec['embed'])
+        self.fc1.weight = PyroSample(
+            dist.Normal(mu,sigma).expand([model_spec['embed'], self.num_features]).to_event(2))
+        self.fc1.bias = PyroSample(
+            dist.Normal(mu,sigma).expand([model_spec['embed']].to_event(1)))
+
+        self.fc2 = PyroModule[nn.Linear](self.num_features, model_spec['embed'])
+        self.fc2.weight = PyroSample(
+            dist.Normal(mu,sigma).expand([2,model_spec['embed']]).to_event(2))
+        self.fc2.bias = PyroSample(
+            dist.Normal(mu,sigma).expand([2].to_event(1)))
+
+        self.model = torch.nn.Sequential(self.fc1,self.fc2)
+
+        self.to(self.device)
+
+    def forward(self, x=None, y=None, seq_len=1):
         if x is None:
-            with pyro.plate("x_plate", seq_len):
-                d_ = dist.Normal(torch.tensor([0.0]).to(self.device), torch.tensor([1.0]).to(self.device)).expand(
-                    [self.num_features]).to_event(1)
-                x = pyro.sample("x", d_)
+            with pyro.plate("x_plant", seq_len):
+                d_ = dist.Normal(torch.tensor([0.0]).to(self.device), 
+                                 torch.tensor([1.0]).to(self.device)).expand([self.num_features]).to_event(1)
+                x = pyro.sample("x",d_)
+
         out = self.model(x)
         mu = out.squeeze()
         softmax = torch.nn.Softmax(dim=1)
-        with pyro.plate("data", out.shape[0]):
+        with pyro.plate("data",out.shape[0]):
             s = softmax(mu)
-            obs = pyro.sample('obs', dist.Categorical(probs=s), obs=y).float()
+            obs = pyro.sample('obs',dist.Categorical(probs=s), obs=y).float()
 
         return x, obs
 
-model = NeuralNetwork(5,2)
-
-assert isinstance(model, nn.Module)
-assert not isinstance(model, PyroModule)
-
-## Convert to Pyro
-
-class PyroLinear(NeuralNetwork, PyroModule):
-    pass
-
-model = PyroLinear(5, 2)
-assert isinstance(model, nn.Module)
-assert isinstance(model, NeuralNetwork)
-assert isinstance(model, PyroModule)
-
-
-
-## parameters
-
-lossFunc = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(),lr=1e-5)
-
-def train(data, model, loss_fn, optimizer):
-    size = len(data)
-    model.train()
-
-    for batch, (X, y) in enumerate(data):
-        X, y = X.to(device), y.to(device)
-
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-def test(data, model, loss_fn):
-    size = len(data)
-    num_batches = len(data)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in data:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
-print("Done!")
-
-
-## Convert to Pyro
-class Bayesian(NeuralNetwork, PyroModule):
-    pass
-
-model = Bayesian(5, 2)
-assert isinstance(model, nn.Module)
-assert isinstance(model, NeuralNetwork)
-assert isinstance(model, PyroModule)
+                
+                                 
